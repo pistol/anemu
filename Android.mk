fb62824ad7f2574382dd0f4e98fb85d1f2529c3d
Valentin Pistol <pistol@cs.duke.edu>
2013-06-22 23:11:04 -0400

    added Android.mk to compile using AOSP build system via 'mm'

--- Android.mk
+++ Android.mk
@@ -1,34 +1,37 @@
+#
+# instructions:
+#
+# 1) symlink anemu and radare2 inside <aosp> tree
+# $ ln -s /path/to/anemu   <aosp>/dalvik/vm/anemu
+# $ ln -s /path/to/radare2 <aosp>/dalvik/vm/anemu/radare2
+#
+# 2) use android build system to build:
+# $ cd <aosp>
+# $ . build/envsetup && lunch_<product>-eng
+# $ cd dalvik/vm/anemu
+# $ mm
+
 LOCAL_PATH := $(call my-dir)
 
-# include $(CLEAR_VARS)
-# LOCAL_MODULE := libr_asm
-# LOCAL_SRC_FILES := ../../arm/radare2/libr/asm/libr_asm.a
-# LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../arm/radare2/libr/include
-# include $(PREBUILT_STATIC_LIBRARY)
+RASM := radare2
 
 include $(CLEAR_VARS)
+MY_PREFIX               := $(LOCAL_PATH)
+MY_SOURCES              := $(wildcard $(MY_PREFIX)/$(RASM)/lib/*.a)
+LOCAL_PREBUILT_LIBS     += $(MY_SOURCES:$(MY_PREFIX)%=%)
+LOCAL_MODULE_TAGS       := optional
+include $(BUILD_MULTI_PREBUILT)
 
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE_TAGS := optional
-
-DARM := $(ANDROID)/arm/darm-v7
-# RASM := $(ANDROID)/arm/radare2/libr
-RASM := $(ANDROID)/arm/radare2/android-install/data/data/org.radare.installer/radare2/
-
-LOCAL_MODULE := anemu 
-LOCAL_SRC_FILES := anemu.c test.S rasm.c
-
-LOCAL_STATIC_LIBRARIES := libdarm
-
-LOCAL_CFLAGS += -gdwarf-2 -g3 -O0 -Wall -march=armv7-a -mcpu=cortex-a9 -marm -mfloat-abi=softfp
-# LOCAL_CFLAGS += -fno-strict-aliasing
-LOCAL_CFLAGS += -I$(DARM)
-LOCAL_CFLAGS += -I$(RASM)/include/libr
-
-# LOCAL_LDFLAGS += -L$(DARM) -ldarm
-LOCAL_LDFLAGS += -L$(RASM)/lib -lr_asm -lr_util -lr_db -lsdb -lr
-
-# TARGET_ARCH_ABI := armeabi-v7a
-# TARGET_PLATFORM := android-14
-
-include $(BUILD_EXECUTABLE)
+include $(CLEAR_VARS)
+LOCAL_MODULE            := libanemu
+LOCAL_MODULE_TAGS       := optional
+LOCAL_REQUIRED_MODULES  := libdarm
+LOCAL_SHARED_LIBRARIES  := liblog
+LOCAL_STATIC_LIBRARIES  := libdarm libr_asm libr_util libr_db libsdb libr
+LOCAL_SRC_FILES         := anemu.c.arm rasm.c
+LOCAL_C_INCLUDES        += dalvik/vm/darm-v7 \
+                           dalvik/vm/anemu/$(RASM)/include/libr
+LOCAL_CFLAGS            += -gdwarf-2 -g3 -O0 -Wall -march=armv7-a -mcpu=cortex-a9
+LOCAL_CFLAGS            += -DANDROID
+# include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
